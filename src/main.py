32bb968878f72c9bd143164246a48113020d3a7f
AJAY POOJITH <146008893+ajaycy@users.noreply.github.com>
2025-01-23 22:21:27 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ def greet():
 
 if __name__ == "__main__":
     print(greet())
-    "aW1wb3J0IGJhc2U2NAoKIyBJbnB1dDogQmFzZTY0IGVuY29kZWQgc3RyaW5nCmVuY29kZWRfc3RyaW5nID0gaW5wdXQoIkVudGVyIHRoZSBCYXNlNjQgZW5jb2RlZCBzdHJpbmc6ICIpCgp0cnk6CiAgICAjIERlY29kZSB0aGUgQmFzZTY0IHN0cmluZwogICAgZGVjb2RlZF9ieXRlcyA9IGJhc2U2NC5iNjRkZWNvZGUoZW5jb2R
-    lZF9zdHJpbmcpCiAgICBkZWNvZGVkX3N0cmluZyA9IGRlY29kZWRfYnl0ZXMuZGVjb2RlKCd1dGYtOCcpICAjIENvbnZlcnQgYnl0ZXMgdG8gc3RyaW5nCiAgICBw
-    cmludCgiRGVjb2RlZCBzdHJpbmc6IiwgZGVjb2RlZF9zdHJpbmcpCmV4Y2VwdCBFeGNlcHRpb24gYXMgZToKICAgIHByaW50KCJFcnJvciBkZWNvZGluZyBCYXNl
+   
     NjQgc3RyaW5nOiIsIGUpCg=="
 #Just a hint or is it an Diversion or are u that weak to find it ?
