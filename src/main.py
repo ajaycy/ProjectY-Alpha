29976387a216c5c9d1b274ad1a43a992594fa224
AJAY POOJITH <146008893+ajaycy@users.noreply.github.com>
2025-01-23 22:02:28 +0530

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,9 @@
+# main.py
+def greet():
+    # Developer note: Removed sensitive function for security reasons.
+    return "Welcome to ProjectX!"
+
+if __name__ == "__main__":
+    print(greet())
+
+#Just a hint or is it an Diversion or are u that weak to find it ?
